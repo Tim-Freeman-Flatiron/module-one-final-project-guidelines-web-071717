af972ffb9ace5058ead047f31475d0565a4f00ff
Tim Freeman <d.timothy.freeman@gmail.com>
2017-08-01 11:54:51 -0400

    made csvadapter and cleaned up seeds file

---
+++ lib/csvadapter.rb
@@ -0,0 +1,42 @@
+module CSVParser
+
+	class Adapter
+		require 'CSV'
+		require 'pry'
+
+		CSV = '../league_data.csv'
+
+		def fill_table_from_csv(csv)
+			League.delete_all
+			Team.delete_all
+			City.delete_all
+			CityLeague.delete_all
+		
+			CSV.foreach(csv) do |row|
+
+				new_city = City.find_or_create_by(name: row[1]) do |city|
+					city.state = row[2]
+				end
+
+				new_league = League.find_or_create_by(name: row[6])
+
+				new_team = Team.create(name: row[0], city_id: new_city.id, league_id: new_league.id)
+			
+				CityLeague.create(city_id: new_city.id, league_id: new_league.id)
+	  
+	  	end
+
+		city_first_row = City.find_by(name: "City")
+		league_first_row = League.find_by(name: "League")
+		team_first_row = Team.find_by(name: "Team")
+
+		city_first_row.destroy
+		league_first_row.destroy
+		team_first_row.destroy
+
+	end
+
+
+	end
+
+end
